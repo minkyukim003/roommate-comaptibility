f2225f6fda6488a20ac01e8c08c690d6ead09956
Alex Golden <agolden2021@gmail.com>
2025-04-12 10:56:51 -0400

    poop

--- roommate-compatibility/app.py
+++ roommate-compatibility/app.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template, request
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import base64
 
+app = Flask(__name__)
+
+questions = [
+    "Cleanliness",
+    "Sleep schedule",
+    "Noise Tolerance",
+    "Guest Frequency",
+    "Communication Style",
+    "Financial Habits",
+    "Pet Friendliness",
+    "Cooking frequency",
+    "Work/Study hours"
+    "Smoking Preferences"
+]
